da21510052feb7889f49a7b1f0630663b43041fb
Rishabh Bansal <rishabh.bansal2200@gmail.com>
2021-10-25 14:47:38 +0530

    first commit

---
+++ springcore/src/main/java/com/springcore/App.java
@@ -0,0 +1,21 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context =
+        		 new ClassPathXmlApplicationContext("config.xml");
+        System.out.println("test1");
+        Student st = (Student)context.getBean("student1");
+        System.out.println(st);
+    }
+}
---
+++ springcore/src/main/java/com/springcore/Student.java
@@ -0,0 +1,37 @@
+package com.springcore;
+
+public class Student {
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
