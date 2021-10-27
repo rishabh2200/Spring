6fc6741355c0e8e5b831bea146cedb53de212ddf
Rishabh Bansal <rishabh.bansal2200@gmail.com>
2021-10-27 11:39:05 +0530

    Quantifier annotation

--- springcore/src/main/java/com/springcore/autowire/Emp.java
+++ springcore/src/main/java/com/springcore/autowire/Emp.java
@@ -3,9 +3,11 @@ package com.springcore.autowire;
 import java.net.Inet4Address;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
 	@Autowired
+	@Qualifier("qwe")
 	private Address address;
 
 	public Address getAddress() {
